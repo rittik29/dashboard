75d2490abeda89f80410ef007f7fd9f66f742ec9
rittik2917 <94971315+rittik29@users.noreply.github.com>
2022-10-04 20:10:49 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,44 @@
+const chart=document.querySelector("#chart").getContext('2d');
+new Chart(chart, {
+    type: 'line',
+    data: {
+        labels: ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'],
+        datasets: [
+        {
+            label: 'BTC',
+            data: [29374,33537,49631,59095,57822,36684,
+            33572,39974,48847,48111,61004],
+            borderColor: 'red',
+            borderWidth: 2
+        },
+        {
+            label: 'ETH',
+            data: [31500,41000,88800,26000,46000,32698,
+            5000,3000,18656,24832,36844],
+            borderColor: 'blue',
+            borderWidth: 2
+        }
+        ]
+    },
+    options: {
+        responsive: true
+    }
+})
+// show or hide sidebar
+const menuBtn=document.querySelector("#menu-btn");
+const closeBtn=document.querySelector("#close-btn");
+const sidebar=document.querySelector("aside");
+menuBtn.addEventListener('click' ,() =>{
+    sidebar.style.display='block';
+})
+closeBtn.addEventListener('click', ()=> {
+    sidebar.style.display='none';
+})
+
+// change theme color
+const themeBtn=document.querySelector('.theme-btn');
+themeBtn.addEventListener('click', ()=> {
+    document.body.classList.toggle('dark-theme');
+    themeBtn.querySelector('span:first-child').classList.toggle('active');
+    themeBtn.querySelector('span:last-child').classList.toggle('active');
+})
